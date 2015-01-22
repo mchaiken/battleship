c03e4de3b5e89f3806aeabff58619a617f1ec3d1
Jenny Baran <jenny.baran@gmail.com>
2015-01-22 10:34:20 -0500

    ship_marker, cleaned up code

--- battleship.c
+++ battleship.c
@@ -1,9 +1,6 @@
 #include "battleship.h"
 #include <unistd.h>
 
-
-
-
 int main(){
     // new_game();
     //printf("Start a New Game! To begin, set up your board.\n\n");
@@ -20,56 +17,54 @@ int main(){
     printf( "Buff: <%s>\n", buff );
     game_over=0;
     
-
-    if(! strcmp(buff,"1") ){
+    if(! strcmp( buff, "1" ) ){
         printf( "Initiating Game\n" );
         initiate_game();
-        my_turn=1;
-        int i=read(socket_client, buff, sizeof(buff));
-        buff[i/sizeof(char)]=0;
-        printf("Recieved <%s>",buff);
-        printf("Send your Info:");
-        fgets(buff,sizeof(buff),stdin);
-        buff[99]=0;
-        write(socket_client,buff, sizeof(buff));
+        my_turn = 1;
+        int i = read( socket_client, buff, sizeof(buff) );
+        buff[ i/sizeof( char ) ] = 0;
+        printf( "Recieved <%s>", buff );
+        printf( "Send your Info:" );
+        fgets( buff, sizeof(buff), stdin );
+        buff[99] = 0;
+        write( socket_client, buff, sizeof(buff) );
     }
+    
     else if(! strcmp(buff, "2") ){
-        printf("socket_server: %d\n",socket_id);
-        printf("enter the IP address you wish to connect to:\n");
-        char ipaddress [100];
-        fgets (ipaddress, sizeof(ipaddress), stdin);
-        join_game(ipaddress);
-        my_turn=0;
-        printf("Send your Info:");
-        fgets(buff,sizeof(buff),stdin);
-        buff[99]=0;
-        write(socket_id,buff, sizeof(buff));
-        i=read(socket_id, buff, sizeof(buff));
-        buff[i/sizeof(char)]=0;
-        printf("Recieved <%s>",buff);
+        printf( "socket_server: %d\n", socket_id );
+        printf( "enter the IP address you wish to connect to:\n" );
+        char ipaddress[100];
+        fgets( ipaddress, sizeof(ipaddress), stdin );
+        join_game( ipaddress );
+        my_turn = 0;
+        printf( "Send your Info:" );
+        fgets( buff, sizeof(buff), stdin );
+        buff[99] = 0;
+        write( socket_id, buff, sizeof(buff) );
+        i = read( socket_id, buff, sizeof(buff) );
+        buff[ i/sizeof( char ) ] = 0;
+        printf( "Recieved <%s>", buff );
     }
     new_game();
     set_board();
-    while(!game_over){
-        if (my_turn){
-            printf("Other player's board\n");
-            print_board(opponent_board);
-            printf("Where do you want to hit?\n");
+    while( !game_over ){
+        if( my_turn ){
+            printf( "Other player's board\n" );
+            print_board( opponent_board );
+            printf( "Where do you want to hit?\n" );
         }
     }
-    
-    
 }
 
 
 void initiate_game(){
-    printf("Intiating Connection\n");
+    printf( "Intiating Connection\n" );
     int socket_id;
     char buffer[256];
     int i, b;
     
     //create the socket
-    socket_id = socket(AF_INET, SOCK_STREAM, 0);
+    socket_id = socket( AF_INET, SOCK_STREAM, 0 );
     
     
     //bind to port/address
@@ -77,21 +72,22 @@ void initiate_game(){
     listener.sin_family = AF_INET;
     listener.sin_port = htons(24601);
     listener.sin_addr.s_addr = INADDR_ANY;
+    
     printf("Waiting for Connection\n");
-    bind( socket_id, (struct sockaddr *)&listener, sizeof(listener));
-    listen(socket_id, 1);
-    socket_client = accept(socket_id, NULL, NULL);
-    printf("Connected\n");
+    bind( socket_id, (struct sockaddr *)&listener, sizeof(listener) );
+    listen( socket_id, 1 );
+    socket_client = accept( socket_id, NULL, NULL );
+    printf( "Connected\n" );
     other_player = socket_client;
 }
 
-void join_game( char * args){
+void join_game( char * args ){
 
     char buffer[256];
     int i, b;
     
     //create the socket
-    socket_id = socket(AF_INET, SOCK_STREAM, 0);
+    socket_id = socket( AF_INET, SOCK_STREAM, 0 );
     
     
     //bind to port/address
@@ -99,11 +95,11 @@ void join_game( char * args){
     sock.sin_family = AF_INET;
     sock.sin_port = htons(24601);
     
-    inet_aton( &args[1], &(sock.sin_addr));
+    inet_aton( &args[1], &(sock.sin_addr) );
     
-    bind( socket_id, (struct sockaddr *)&sock, sizeof(sock));
-    i=connect(socket_id,(struct sockaddr *)&sock, sizeof(sock));
-    printf("Connected\n");
+    bind( socket_id, (struct sockaddr *)&sock, sizeof(sock) );
+    i = connect( socket_id,(struct sockaddr *)&sock, sizeof(sock) );
+    printf( "Connected\n" );
     other_player = socket_id;
 }
 
@@ -149,7 +145,7 @@ void print_board( char board[] ){
 //place ship in array
 int check_collisions( int len, int increment, int i ){
     while( len ){
-        if( your_board[i] == 'O' ){
+        if( your_board[i] != '-' ){
             return 0;
         }
         i = i + increment;
@@ -173,7 +169,7 @@ int get_i (){
 
 void alter_array( int len, int increment, int i ){
     while( len ){
-        your_board[i] = 'O';
+        your_board[i] = ship_marker;
         i = i + increment;
         len = len - 1;
     }
@@ -238,7 +234,7 @@ void place_ship( int len ){
     }
 }
 
-void set_board (){
+void set_board () {
     print_board( your_board );
     
     place_ship(6);
